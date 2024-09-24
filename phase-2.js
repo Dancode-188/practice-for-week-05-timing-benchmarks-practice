d8117fb4dbf518e100e8b267ffb9aed3c2a3d88d
Dancode-188 <danbitengo@gmail.com>
2024-09-24 05:51:59 +0300

    Refactor addNums10 and addManyNums10 functions in phase-2.js

--- phase-2.js
+++ phase-2.js
@@ -3,12 +3,21 @@ const [addNums, addManyNums] = require("./phase-1");
 // Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
   // Fill this in
-
+  let sums = [];
+  for (let i = 1; i <= 10; i++) {
+    sums.push(addNums(i * increment));
+  }
+  return sums;
 }
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
   // Fill this in
+  let sums = [];
+  for (let i = 1; i <= 10; i++) {
+    sums.push(addManyNums(i * increment));
+  }
+  return sums;
 
 }
 
