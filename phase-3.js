fadc060189f92af5b6305e4fa5a948e213db3833
Dancode-188 <danbitengo@gmail.com>
2024-09-24 06:45:38 +0300

    Refactor timing code for addNums10Timing and addManyNums10Timing functions

--- phase-3.js
+++ phase-3.js
@@ -5,6 +5,20 @@ function addNums10Timing(increment) {
   // Then, add timing code
 
   // Your code here
+  console.time("Total time for addNums10Timing");
+  let sums = [];
+  for (let i = 1; i <= 10; i++) {
+    let startTime = Date.now();
+    console.time(`addNums call ${i}`);
+    sums.push(addNums(i * increment));
+    console.timeLog(`addNums call ${i}`);
+    let endTime = Date.now();
+    console.log(`addNums call ${i * increment} took ${endTime - startTime}ms`);
+    console.timeEnd(`addNums call ${i}`);
+  }
+  console.timeEnd("Total time for addNums10Timing");
+
+  return sums;
 
 }
 
@@ -14,6 +28,19 @@ function addManyNums10Timing(increment) {
 // Then, add timing code
 
   // Your code here
+  console.time("Total time for addManyNums10Timing");
+  let sums = [];
+  for (let i = 1; i <= 10; i++) {
+    let startTime = Date.now();
+    console.time(`addManyNums call ${i}`);
+    sums.push(addManyNums(i * increment));
+    console.timeLog(`addManyNums call ${i}`);
+    let endTime = Date.now();
+    console.log(`addManyNums call ${i * increment} took ${endTime - startTime}ms`);
+    console.timeEnd(`addManyNums call ${i}`);
+  }
+  console.timeEnd("Total time for addManyNums10Timing");
+  return sums;
 
 }
 
