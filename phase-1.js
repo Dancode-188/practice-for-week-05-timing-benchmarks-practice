919d57d955dc5bc0a1811249dff0263319cff45d
Dancode-188 <danbitengo@gmail.com>
2024-09-24 05:35:12 +0300

    Refactor addNums and addManyNums functions in phase-1.js

--- phase-1.js
+++ phase-1.js
@@ -1,6 +1,11 @@
 // Adds up positive integers from 1-n
 function addNums(n) {
   // Fill this in
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
 
 }
 
@@ -8,6 +13,11 @@ function addNums(n) {
 // Adds up values of addNums(1) through addNums(n)
 function addManyNums(n) {
   // Fill this in
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += addNums(i);
+  }
+  return sum;
 
 }
 
